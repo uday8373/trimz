3837faddde04736d51c5502c3ba69674580602f0
Uday Kumar <37672803+uday8373@users.noreply.github.com>
2024-07-03 22:18:00 +0530

    Update shorten.js allow all origin

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -6,6 +6,17 @@ import {VerifyToken} from "../../utils/VerifyToken";
 import rateLimit from "../../utils/RateLimit";
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
   await DbConnect();
   const RESERVED_TRIMZ_LINKS = [
     "erex",
